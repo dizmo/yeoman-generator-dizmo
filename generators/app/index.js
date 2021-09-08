74b4672aa70d0947512f1396d964972506a82cb7
Hasan Karahan <hasan.karahan@blackhan.com>
2021-09-08 16:19:11 +0300

    DIG-418: migrate public properties

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,6 +11,7 @@ const process = require('process');
 const shell = require('shelljs');
 const rimraf = require('rimraf');
 const yosay = require('yosay');
+const { matchRecursive: xmatch } = require('xregexp');
 
 module.exports = class extends Generator {
     constructor(args, opts) {
@@ -574,22 +575,35 @@ module.exports = class extends Generator {
         }
     }
     async _mig() {
-        const rx = (method) => {
-            return new RegExp(`(dizmo|bundle|viewer)\.${method}`, 'g');
+        const rx = (method, flags = 'g') => {
+            return new RegExp(`(dizmo|bundle|viewer)\.${method}`, flags);
         };
-        const rx_private = (method) => {
-            return new RegExp(`(dizmo|bundle|viewer)\.privateStorage\.${method}`, 'g');
+        const rx_public = (method, flags = 'g') => {
+            return new RegExp(`(dizmo|bundle|viewer)\.publicStorage\.${method}`, flags);
         };
+        const rx_private = (method, flags = 'g') => {
+            return new RegExp(`(dizmo|bundle|viewer)\.privateStorage\.${method}`, flags);
+        };
+        //
+        // unsubscriptions
+        //
         await walk(this.destinationPath('source'), (filepath) => {
             const script = fs
                 .readFileSync(this.destinationPath(filepath), 'utf8')
-                // unsubscriptions
                 .replace(rx('unsubscribeRemoteHost'), '$1.unsubscribe')
                 .replace(rx('unsubscribeParentChange'), '$1.unsubscribe')
                 .replace(rx('unsubscribeChildren'), '$1.unsubscribe')
                 .replace(rx('unsubscribeDizmoChanged'), '$1.unsubscribe')
-                .replace(rx('unsubscribeBundleChanged'), '$1.unsubscribe')
-                // attributes
+                .replace(rx('unsubscribeBundleChanged'), '$1.unsubscribe');
+            fs.writeFileSync(
+                this.destinationPath(filepath), script);
+        });
+        //
+        // attributes
+        //
+        await walk(this.destinationPath('source'), (filepath) => {
+            const script = fs
+                .readFileSync(this.destinationPath(filepath), 'utf8')
                 .replace(rx('getAttribute'), '$1.getAttribute')
                 .replace(rx('setAttribute'), '$1.setAttribute')
                 .replace(rx('deleteAttribute'), '$1.deleteAttribute')
@@ -599,8 +613,16 @@ module.exports = class extends Generator {
                 .replace(rx('beginAttributeUpdate'), '$1.beginAttributeUpdate')
                 .replace(rx('endAttributeUpdate'), '$1.endAttributeUpdate')
                 .replace(rx('cacheAttribute'), '$1.cacheAttribute')
-                .replace(rx('uncacheAttribute'), '$1.uncacheAttribute')
-                // private properties
+                .replace(rx('uncacheAttribute'), '$1.uncacheAttribute');
+            fs.writeFileSync(
+                this.destinationPath(filepath), script);
+        });
+        //
+        // private properties
+        //
+        await walk(this.destinationPath('source'), (filepath) => {
+            const script = fs
+                .readFileSync(this.destinationPath(filepath), 'utf8')
                 .replace(rx_private('getProperty'), '$1.getProperty')
                 .replace(rx_private('setProperty'), '$1.setProperty')
                 .replace(rx_private('deleteProperty'), '$1.deleteProperty')
@@ -611,8 +633,64 @@ module.exports = class extends Generator {
                 .replace(rx_private('cacheProperty'), '$1.cacheProperty')
                 .replace(rx_private('uncacheProperty'), '$1.uncacheProperty');
             fs.writeFileSync(
-                this.destinationPath(filepath), script
-            );
+                this.destinationPath(filepath), script);
+        });
+        //
+        // public properties
+        //
+        await walk(this.destinationPath('source'), (filepath) => {
+            const replace = (script, method, new_method) => {
+                let m, re = rx_public(method);
+                while (
+                    (m = re.exec(script)) !== null
+                ) {
+                    const at = m.index + m[0].length;
+                    try {
+                        const xm = xmatch(script.slice(at), '\\(', '\\)', '', {
+                            valueNames: ['between', 'left', 'match', 'right']
+                        });
+                        if (xm && xm.length) {
+                            const [l, m, r] = xm;
+                            if (l && l.name === 'left' &&
+                                m && m.name === 'match' &&
+                                r && r.name === 'right'
+                            ) {
+                                script = script.slice(0, at)
+                                    + l.value // opening parenthesis
+                                    + m.value // list of parameter(s)
+                                    + ', { public: true }' // public
+                                    + r.value // closing parenthesis
+                                    + script.slice(at + r.end);
+                                script = script
+                                    .replace(rx_public(method, ''), new_method);
+                            }
+                        }
+                    } catch (ex) {
+                        console.log(`{0} failed rewriting {1} at {2}; add a missing {3} flag!`
+                            .replace('{3}', chalk.white.bold('{ public: true }'))
+                            .replace('{0}', chalk.yellow.bold('[WARN]'))
+                            .replace('{1}', chalk.white.bold(m[0]))
+                            .replace('{2}', `${filepath}:${at}`)
+                        );
+                        script = script
+                            .replace(rx_public(method, ''), new_method);
+                    }
+                }
+                return script;
+            };
+            let script = fs
+                .readFileSync(this.destinationPath(filepath), 'utf8');
+            script = replace(script, 'getProperty', '$1.getProperty');
+            script = replace(script, 'setProperty', '$1.setProperty');
+            script = replace(script, 'deleteProperty', '$1.deleteProperty');
+            script = replace(script, 'subscribeToProperty', '$1.onProperty');
+            script = replace(script, 'unsubscribeProperty', '$1.unsubscribe');
+            script = replace(script, 'beginPropertyUpdate', '$1.beginPropertyUpdate');
+            script = replace(script, 'endPropertyUpdate', '$1.endPropertyUpdate');
+            script = replace(script, 'cacheProperty', '$1.cacheProperty');
+            script = replace(script, 'uncacheProperty', '$1.uncacheProperty');
+            fs.writeFileSync(
+                this.destinationPath(filepath), script);
         });
     }
     async _rim() {
